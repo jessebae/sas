b994bf58a9734bb5e7e93f3bd8d8ae9a00789466
Steve Stout <jessebae@sbcglobal.net>
2015-08-07 02:06:07 +0000

    Re-did Mailer and View

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,8 @@ class ContactsController < ApplicationController
     end
     
     def create
-        @contact = Contact.new(contact_params)
-    if @contact.save
+      @contact = Contact.new(contact_params)
+      if @contact.save
         name = params[:contact][:name]
         email = params[:contact][:email]
         body = params[:contact][:comments]
@@ -13,10 +13,10 @@ class ContactsController < ApplicationController
         ContactMailer.contact_email(name, email, body).deliver
         flash[:success] = 'Message sent.'
         redirect_to new_contact_path
-    else
+      else
         flash[:danger] = 'Error occured, message has not been sent.'
         redirect_to new_contact_path
-    end
+      end
     end
     
     private
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,4 +1,5 @@
 class ContactMailer < ActionMailer::Base
+    default to: 'jessebae@sbcglobal.net'
     
     def contact_email(name, email, body)
         @name = name
